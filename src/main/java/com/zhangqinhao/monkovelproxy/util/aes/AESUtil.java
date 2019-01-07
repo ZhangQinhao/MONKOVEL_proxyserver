a8ed98b11a164539889243917869c30a5f5b13b7
1105075896@qq.com <zqh19931118>
2019-01-08 02:11:46 +0800

    ·

--- src/main/java/com/zhangqinhao/monkovelproxy/util/aes/AESUtil.java
+++ src/main/java/com/zhangqinhao/monkovelproxy/util/aes/AESUtil.java
@@ -1,46 +1,102 @@
 package com.zhangqinhao.monkovelproxy.util.aes;
 
-import com.zhangqinhao.monkovelproxy.util.ParseSystemUtil;
-
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
+import java.security.SecureRandom;
 
 public class AESUtil {
-    public static String aesEncode(String cleartext, String seed) throws Exception {
-        byte[] rawKey = deriveKeyInsecurely(seed, 32).getEncoded();
-        byte[] result = encrypt(rawKey, cleartext.getBytes());
-        return ParseSystemUtil.parseByte2HexStr(result);
+    public static String aesEncode(String seed, String clearText) {
+        byte[] result = null;
+
+        try {
+            byte[] rawkey = getRawKey(seed.getBytes());
+            result = encrypt(rawkey, clearText.getBytes());
+        } catch (Exception var4) {
+            var4.printStackTrace();
+        }
+
+        if (result != null) {
+            String content = toHex(result);
+            return content;
+        } else {
+            return null;
+        }
     }
 
-    public static String aesDecode(String encrypted, String seed) throws Exception {
-        byte[] rawKey = deriveKeyInsecurely(seed, 32).getEncoded();
-        byte[] enc = ParseSystemUtil.parseHexStr2Byte(encrypted);
-        byte[] result = decrypt(rawKey, enc);
-        return new String(result);
+    public static String aesDecode(String seed, String encrypted) {
+        try {
+            byte[] rawKey = getRawKey(seed.getBytes());
+            byte[] enc = toByte(encrypted);
+            byte[] result = decrypt(rawKey, enc);
+            String coentn = new String(result);
+            return coentn;
+        } catch (Exception var6) {
+            return null;
+        }
+    }
+
+    private static byte[] getRawKey(byte[] seed) throws Exception {
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "Crypto");
+        sr.setSeed(seed);
+        kgen.init(128, sr);
+        SecretKey sKey = kgen.generateKey();
+        byte[] raw = sKey.getEncoded();
+        return raw;
     }
 
     private static byte[] encrypt(byte[] raw, byte[] clear) throws Exception {
         SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(1, skeySpec, new IvParameterSpec(new byte[cipher.getBlockSize()]));
         byte[] encrypted = cipher.doFinal(clear);
         return encrypted;
     }
 
     private static byte[] decrypt(byte[] raw, byte[] encrypted) throws Exception {
         SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(2, skeySpec, new IvParameterSpec(new byte[cipher.getBlockSize()]));
         byte[] decrypted = cipher.doFinal(encrypted);
         return decrypted;
     }
 
-    private static SecretKey deriveKeyInsecurely(String password, int keySizeInBytes) throws UnsupportedEncodingException {
-        byte[] passwordBytes = password.getBytes("UTF-8");
-        return new SecretKeySpec(
-                InsecureSHA1PRNGKeyDerivator.deriveInsecureKey(
-                        passwordBytes, keySizeInBytes),
-                "AES");
+    public static String toHex(String txt) {
+        return toHex(txt.getBytes());
+    }
+
+    public static String fromHex(String hex) {
+        return new String(toByte(hex));
+    }
+
+    public static byte[] toByte(String hexString) {
+        int len = hexString.length() / 2;
+        byte[] result = new byte[len];
+
+        for(int i = 0; i < len; ++i) {
+            result[i] = Integer.valueOf(hexString.substring(2 * i, 2 * i + 2), 16).byteValue();
+        }
+
+        return result;
+    }
+
+    public static String toHex(byte[] buf) {
+        if (buf == null) {
+            return "";
+        } else {
+            StringBuffer result = new StringBuffer(2 * buf.length);
+
+            for(int i = 0; i < buf.length; ++i) {
+                appendHex(result, buf[i]);
+            }
+
+            return result.toString();
+        }
+    }
+
+    private static void appendHex(StringBuffer sb, byte b) {
+        String HEX = "0123456789ABCDEF";
+        sb.append("0123456789ABCDEF".charAt(b >> 4 & 15)).append("0123456789ABCDEF".charAt(b & 15));
     }
 }
